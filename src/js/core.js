0fcb6a3685676d6a225cd57dbc8621606c473b99
Andreas Blaesius <andi@unlegacy-android.org>
2020-12-06 12:48:41 +0100

    live chroma: add reload button, move button functions out of core.js, fix lint errors
    Change-Id: I674f661a4ad9cfa80cb162fd13a04d393b360b41

--- src/js/core.js
+++ src/js/core.js
@@ -474,6 +474,7 @@ const photoBooth = (function () {
                         $('.loading').append($('<a class="btn" style="margin-left:2px" href="./">').text(abortmsg));
                     }
                 } else if (result.success === 'chroma') {
+                    chromaFile = result.file;
                     api.processPic(data.style, result);
                 } else {
                     currentCollageFile = '';
@@ -884,14 +885,6 @@ const photoBooth = (function () {
 
         api.thrill('photo');
         $('.newpic').blur();
-    });
-
-    // Take Chroma Button
-    $('.takeChroma, .newchroma').on('click', function (e) {
-        e.preventDefault();
-
-        api.thrill('chroma');
-        $('.takeChroma').blur();
     });
 
     // Take Collage Button
